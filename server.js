49aa115b87a394e8a47c7cedd32700e0282b61fc
Glitch (wild-toy) <none>
2018-02-19 04:06:19 +0000

    🦄🐚 Updated with Glitch

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var fs = require('fs'),
     express = require('express'),
     app = express(),   
     Twit = require('twit'),
+    Fuse = require('fuse.js'),
+    HashMap = require('hashmap'),
     config = {
     /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/make-an-image-posting-twitter-bot/#creating-a-twitter-app*/      
       twitter: {
@@ -16,20 +18,12 @@ var fs = require('fs'),
     T = new Twit(config.twitter),
     stream = T.stream('statuses/sample');
 
-var bot_responses = [
-  "I am awake!",
-  "I'm awake!",
-  "I dozed off.",
-  "I dozed off, but I am awake now!",
-  "I dozed off, but I'm awake now!"
-];
-
 var data = [
   //[Name, Contribution, Twitter handle, political opponent, opponent handle]
   //From https://www.nytimes.com/interactive/2017/10/04/opinion/thoughts-prayers-nra-funding-senators.html
   //Sens
-  ["John McCain", "$7,740,520", ''],
-  ["Richard Burr", "$6,986,620", ''],
+  ["John McCain", "7,740,520", ''],
+  ["Richard Burr", "6,986,620", ''],
   ['Roy Blunt', '4,551,146', ''],
   ['Thom Tillis', '4,418,012', ''],
   ['Cory Gardner', '3,879,064', ''],
@@ -51,17 +45,88 @@ var data = [
   ['Barbara Comstock', '137,232', ''],
 ];
 
-// Returns an index into the politician table
-function parse_for_politician(text) {
-  return 0;
+var data2 = [{'contribution': '7,740,520', 'name': 'John McCain'}, {'contribution': '6,986,620', 'name': 'Richard Burr'}, {'contribution': '4,551,146', 'name': 'Roy Blunt'}, {'contribution': '4,418,012', 'name': 'Thom Tillis'}, {'contribution': '3,879,064', 'name': 'Cory Gardner'}, {'contribution': '3,303,355', 'name': 'Marco Rubio'}, {'contribution': '3,124,273', 'name': 'Joni Ernst'}, {'contribution': '3,061,941', 'name': 'Rob Portman'}, {'contribution': '2,896,732', 'name': 'Todd Young'}, {'contribution': '2,861,047', 'name': 'Bill Cassidy'}, {'contribution': '1,089,477', 'name': 'French Hill'}, {'contribution': '800,544', 'name': 'Ken Buck'}, {'contribution': '707,662', 'name': 'David Young'}, {'contribution': '385,731', 'name': 'Mike Simpson'}, {'contribution': '344,630', 'name': 'Greg Gianforte'}, {'contribution': '245,720', 'name': 'Don Young'}, {'contribution': '221,736', 'name': 'Lloyd Smucker'}, {'contribution': '201,398', 'name': 'Bruce Poliquin'}, {'contribution': '158,111', 'name': 'Pete Sessions'}, {'contribution': '137,232', 'name': 'Barbara Comstock'}];
+var options = {
+  shouldSort: true,
+  findAllMatches: true,
+  includeScore: true,
+  includeMatches: true,
+  threshold: 0.3,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 3,
+  keys: [
+    "name"
+  ]
+};
+var fuse = new Fuse(data2, options);
+
+//i think id tokenize and search each token of the string, threshold it, assemble a vote, take the vote winner
+function find_poli(toks) {
+  var voteMap = new HashMap();
+  var poliMap = new HashMap();
+  
+  for (var i = 0; i < toks.length; i++) {
+    var t = toks[i];
+    var results = fuse.search(t);
+    for (var j = 0; j < results.length; j++) {
+      var res = results[j];
+      if (res['score'] > 0.3 || res['matches'].length == 0) //Since list is sorted, scores only get worse
+        break;
+      //console.log(res);
+      var poli = res['item'];
+      if (voteMap.has(poli['name'])) {
+        voteMap.set(poli['name'], voteMap.get(poli['name'])+1);
+      } else {
+        voteMap.set(poli['name'], 1);
+        poliMap.set(poli['name'], poli);
+      }
+    }
+  }
+  
+  var votes = voteMap.entries();
+  votes.sort(function(a, b) { return b[1] - a[1]; });
+  console.log(votes);
+  
+  if (votes.length === 0)
+    return null;
+  if (votes.length > 1) {
+    if (votes[0][1] === votes[1][1])
+      return null;
+  }
+  var poli = poliMap.get(votes[0][0]);
+  return poli;
+}
+
+function tokenize_and_normalize(msg) {
+  var arr = [];
+  var toks = msg.split(' ');
+  for (var i = 0; i < toks.length; i++) {
+    var t = toks[i];
+    if (t.indexOf(process.env.TWITTER_HANDLE) !== -1)
+      continue;
+    t = t.replace(/[^\w\s]|_/g, "");
+    arr.push(t);
+  }
+  return arr;
 }
 
 function get_response(event){
   console.log(event.text);
   
-  var poli = parse_for_politician(event.text);
-  var res = 'The NRA contributed ' + data[poli][1] + ' to ' + data[poli][0] + ' ' + data[poli][2] + ' (according to opensecrets.org).';
+  var toks = tokenize_and_normalize(event.text);
+  console.log(toks);
   
+  var poli = find_poli(toks);
+  console.log(poli);
+  
+  if (poli !== null) {
+    var res = 'The NRA contributed $' + poli['contribution'] + ' to ' + poli['name'] + ' ' + '' + ' (according to https://www.opensecrets.org/orgs/summary.php?id=d000000082).';
+  } else {
+    var res = "I don't know who you're asking about. I might not have that person in my database. Or you can try giving me the politician's full name.";
+  }
+
   return res; 
 }
 
@@ -83,6 +148,9 @@ app.all("/tweet", function (request, response) {
           console.log(status.id_str);
           console.log(status.text);
           console.log(status.user.screen_name);
+          
+          if (status.user.screen_name === 'iamawakebot')
+            return;
           
           var text = get_response(status);
 
